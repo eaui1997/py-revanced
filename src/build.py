a3cacbfd80a51108c30e9c17f94c48ff615e512c
manhduonghn <manhduonghn@live.com>
2023-08-07 22:06:43 +0700

    Update version apk

--- src/build.py
+++ src/build.py
@@ -52,7 +52,7 @@ class Build(object):
                 "--apk",
                 input_apk_filepath,
                 "--out",
-                f"./{config['dist_dir']}/{target_app}-revanced.apk",
+                f"./{config['dist_dir']}/{target_app}-revanced-{version}.apk",
                 "--merge",
                 self.download_files["revanced-integrations"],
                 "--keystore",
@@ -86,7 +86,7 @@ class Build(object):
             logger.error("An error occurred while running the Java program")
             sys.exit(1)
 
-        output_path = f"./revanced-cache/{target_app}-revanced_signed.apk"
+        output_path = f"./revanced-cache/{target_app}-revanced-{version}_signed.apk"
 
         # Check if the output file exists
         if not os.path.exists(output_path):
