8d6034ca0f9396897b9547bc3e1cec0ad8c2f66d
manhduonghn <manhduonghn@live.com>
2023-08-30 16:49:09 +0700

    Update build.py

--- src/build.py
+++ src/build.py
@@ -1,49 +1,65 @@
-def run_build(self):
-    target_app = self.args.app_name.lower().strip()
-    input_apk_filepath = Downloader().download_apk(target_app)
+import os
+import subprocess
+import sys
+from loguru import logger
+from src._config import config
+from src.downloader import Downloader
+
+class Build(object):
+    def __init__(self, args):
+        if not os.path.exists(config["dist_dir"]):
+            os.mkdir(config["dist_dir"])
+        
+        self.args = args
+        self.check_java_version()
+        self.download_files = Downloader().download_required()
+        self.exclude_patches: str | None = self.args.exclude_patches
+        self.include_patches: str | None = self.args.include_patches
+
+    def run_build(self):
+        target_app = self.args.app_name.lower().strip()
+        input_apk_filepath = Downloader().download_apk(target_app)
     
-    logger.info(f"Running build for {target_app}:")
+        logger.info(f"Running build for {target_app}:")
+     
+        exclude_patches = sum(
+            [["--exclude", s.strip()] for s in self.args.exclude_patches.split(",")],
+            [],
+        )
     
-    exclude_patches = sum(
-        [["--exclude", s.strip()] for s in self.args.exclude_patches.split(",")],
-        [],
-    )
+        include_patches = sum(
+            [["--include", s.strip()] for s in self.args.include_patches.split(",")],
+            [],
+        )
     
-    include_patches = sum(
-        [["--include", s.strip()] for s in self.args.include_patches.split(",")],
-        [],
-    )
+        process = subprocess.Popen(
+            [
+                "java",
+                "-jar",
+                self.download_files["revanced-cli"],
+                "patch",
+                "-b",
+                self.download_files["revanced-patches"],
+                "-o",
+                f"{target_app}-revanced.apk",
+                "-m",
+                self.download_files["revanced-integrations"],
+                "--keystore",
+                config["keystore_path"],
+                *exclude_patches,
+                *include_patches,
+                 input_apk_filepath,
+            ],
+            stdout=subprocess.PIPE,
+        )
     
-    process = subprocess.Popen(
-        [
-            "java",
-            "-jar",
-            self.download_files["revanced-cli"],
-            "patch",
-            "-b",
-            self.download_files["revanced-patches"],
-            "-o",
-            f"./{config['dist_dir']}/{target_app}-revanced.apk",
-            "-m",
-            self.download_files["revanced-integrations"],
-            "--keystore",
-            config["keystore_path"],
-            *exclude_patches,
-            *include_patches,
-            input_apk_filepath,
-        ],
-        stdout=subprocess.PIPE,
-    )
+        output = process.stdout
     
-    output = process.stdout
+        for line in output:
+            print(line.decode("utf-8"), end="")
     
-    for line in output:
-        print(line.decode("utf-8"), end="")
+        if process.wait() != 0:
+            logger.error("An error occurred while running the Java program")
+            sys.exit(1)
     
-    if process.wait() != 0:
-        logger.error("An error occurred while running the Java program")
-        sys.exit(1)
-    
-    # 不验证 output_path
-    
-    logger.success(f"Build completed successfully")
+        logger.success(f"Build completed successfully")
