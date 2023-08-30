93afcc95c9cce50a0178fc4e2a2d88112c5ad07c
manhduonghn <manhduonghn@live.com>
2023-08-30 18:57:49 +0700

    Update build.py

--- src/build.py
+++ src/build.py
@@ -41,7 +41,7 @@ class Build(object):
                 "-b",
                 self.download_files["revanced-patches"],
                 "-o",
-                f"{target_app}-revanced.apk",
+                f"{target_app}-revanced-v{version}.apk",
                 "-m",
                 self.download_files["revanced-integrations"],
                 "--keystore",
